0db3b166a8774d0aab8438d8fb33bdd8be45fc31
gnkatchmar <gunk55@msn.com>
2016-11-29 13:59:16 -0800

    1129-1400 basic working

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,14 @@
+const HtmlWebpackPlugin = require( 'html-webpack-plugin' );
+
 module.exports = {
   entry: './src/main.js',
   output: {
     path: './build',
-    filename: 'main.js'
+    filename: 'bundle.js'
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
 };
