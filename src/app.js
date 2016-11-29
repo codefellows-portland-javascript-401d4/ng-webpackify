0ab976aeedd8a0a218945c62e6388c46442c1235
caraldi <araldicm@Caitlin-M-Araldi.local>
2016-11-29 15:30:35 -0800

    + styling

--- src/app.js
+++ src/app.js
@@ -1,17 +1,15 @@
 'use strict';
 
 const angular = require('angular');
-const reverse = require('./reverse');
 require('./main.css');
 
 const app = angular.module('caraldiApp', []);
 
-app.controller('messageControl', function($scope) {
+app.controller('mainController', function($scope) {
     $scope.styles = ['fatface', 'lobster', 'condensed', 'oswald'];
-    $scope.style = 'condensed';
+    $scope.style = 'fatface';
     $scope.fontSize = 22;
     
     $scope.message = 'hello';
     $scope.name = 'world';
-    $scope.reverse = reverse;
 });
--- src/reverse.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-module.exports = function reverse(string='hello world') {
-    return string.split('').reverse().join('');
-};
