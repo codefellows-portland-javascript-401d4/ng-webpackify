a40ee2f77f42ad54dc7828cb1ffaf4ff51af4db1
gnkatchmar <gunk55@msn.com>
2016-11-29 13:08:59 -0800

    1129-1308 intial file prep

---
+++ src/main.js
@@ -0,0 +1,8 @@
+var app = angular.module('myApp', []);
+
+app.controller('formCtrl', function($scope) {
+  $scope.fonts = ['Courier New', 'Helvetica', 'Impact', 'Times New Roman', 'Verdana'];
+  $scope.styles = ['fancy', 'plain', 'modern'];
+  $scope.style = 'plain';
+  $scope.message = 'hello world';
+});
