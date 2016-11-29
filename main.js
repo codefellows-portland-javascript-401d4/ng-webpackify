d1da93265f87332e9803db16eb69c04498e091e3
Gloria Guy <totallygloria@gmail.com>
2016-11-28 16:55:01 -0800

    Finally figured out how to grab the selected value out of a drop down and respond accordingly. Requires ng-change as far as I can tell.

--- main.js
+++ main.js
@@ -3,5 +3,19 @@
 var helloApp = angular.module('helloApp', []);
 
 helloApp.controller('helloController', function($scope) {
-  $scope.greeting = 'Hola!';
+
+  $scope.languages = ['English', 'Spanish', 'French', 'Japanese'];
+
+  $scope.selected = function(lang) {
+    if (lang === 'English') {
+      $scope.greeting = 'Hello!';
+    } else if (lang === 'Spanish') {
+      $scope.greeting = 'Hola!';
+    } else if (lang === 'French') {
+      $scope.greeting = 'Bon jour!';
+    } else if (lang === 'Japanese') {
+      $scope.greeting = 'Konnichiwa!';
+    }
+  }
+
 });
