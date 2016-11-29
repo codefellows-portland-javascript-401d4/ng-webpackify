f74180ba78296f6864f533f57e46d5f1bb8362aa
Albert Reel <waxhoya@gmail.com>
2016-11-29 13:52:18 -0800

    adding loaders

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+
+
+module.exports = {
+
+    entry: './main.js',
+    output: {
+        path: './',
+        filename: "bundle.js"
+    },
+    module: {
+        preloaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/
+            loader: 'eslint-loader'
+        }],
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/
+            loader: 'babel-loader'
+        }]
+    }
+
+}
