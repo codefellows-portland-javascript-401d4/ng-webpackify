04dbf1dc3cdb900d02107a5802bd2474c0a93c79
Albert Reel <waxhoya@gmail.com>
2016-11-29 20:28:41 -0800

    made linter happy

--- src/app.js
+++ src/app.js
@@ -4,8 +4,8 @@ require('./style.css');
 var app = angular.module('myApp', []);
 
 app.controller('formMsg', function($scope) {
-    $scope.message = "default";
+    $scope.message = 'default';
     $scope.fontRange = 3;
     $scope.styles = ['spicy','cooling','indignant'];
-    $scope.custStyle = 'spicy'
+    $scope.custStyle = 'spicy';
 });
